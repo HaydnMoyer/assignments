3cd83eede5fd8131cec44f3ed443728d559325d6
HaydnMoyer <haydnmoyer0520@icloud.com>
2026-09-22 13:53:04 -0400

    4

---
+++ 04_working_with_lists/04_10_slice.py
@@ -0,0 +1,12 @@
+''' 
+Chapter 4 slice
+haydn moyer
+'''
+
+'''
+Lists
+'''
+heroes = ['superman', 'batman', 'spiderman', 'wonder woman',
+           'flash']
+
+print(heroes[1:3], "are the best ones.")
